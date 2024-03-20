961e6868515f59c087b668951ee50e206e22ee43
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-03-20 14:12:35 +0200

    Completed 5 and added comments

--- Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
+++ Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
@@ -13,36 +13,25 @@ console.log(cansLeft < 5 ? "Time to restock!" : "We're stocked!");  // Print bas
 
 // #### Challenge 3: Workout Intensity
 // Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
-// If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
+// If the heart rate is below 100 bpm, print "Boost needed!", otherwise, prints "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
 console.log(heartRate < 100 ? "Boost needed!" : "Energy Levels are High!");
-//Checks if heartRate is less than 100 and prints "Boost needed!", or "Energy levels are high!"
+//Checks if heartRate is less than 100 and prints "Boost needed!", or prints "Energy levels are high!"
 
 
 // #### Challenge 4: Temperature Suitability
 let currentTemp = 4; // Current temperature in °C
 console.log(currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cooler!"); //Print based on temperature
-//Checks if temparature is below or equal then prints "Chilled to perfection!" or "Needs a cooler!" 
+//Checks if temparature is below or equal then prints "Chilled to perfection!" or prints "Needs a cooler!" 
 
 
 // #### Challenge 5: Late Night Coding Session
-// Determine if it's a good idea to have a Monster Energy drink based on the current hour.
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!",
-// otherwise, print "Better stick to water."
-
-// Define the current hour in 24-hour format
-let currentHour = 22; 
-
-// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
-// Otherwise, print "Better stick to water."
+let currentHour = 22;
+console.log((currentHour >= 7 && currentHour < 24) ? "Unleash the beast!" : "Better Stick to water."); 
+//Checks if currentHour is between 7 (inclusive) and 24 (exclusive) and prints "Unleash the beast!", or prints "Better stick to water.". Note that < 24 ensures it doesn't include midnight itself.
 
 
 
 
 
-
-// In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
-// demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct, 
-// they should be used judiciously to maintain readability, especially for more complex conditions.
