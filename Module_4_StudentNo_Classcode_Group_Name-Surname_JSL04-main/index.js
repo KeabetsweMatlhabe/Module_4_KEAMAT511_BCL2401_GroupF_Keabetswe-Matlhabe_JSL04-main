f2246280be137619a87c12137f5c74d984229823
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-03-20 13:37:39 +0200

    Completed challenge 1 and 2

---
+++ Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
@@ -0,0 +1,49 @@
+// # [JSL04] Submission: Monster Ternary Operator
+
+// #### Challenge 1: Flavor Selection
+let userPreference = 'Sugar-free'; 
+console.log(userPreference ? userPreference : 'Regular'); 
+//Checks if userPreference is truthy (not undefined) and prints it, otherwise defaults to "Regular".
+
+
+// #### Challenge 2: Stock Check
+let cansLeft = 3; // Any number of cans
+console.log(cansLeft < 5 ? "Time to restock!" : "We're stocked!");  // Print based on can count
+
+
+// #### Challenge 3: Workout Intensity
+// Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
+// If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
+
+let heartRate = 95; // Current heart rate in bpm
+console.log(`'}`);
+
+
+// #### Challenge 4: Temperature Suitability
+// Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
+// if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
+
+let currentTemp = 4; // Current temperature in °C
+console.log(``);
+
+
+// #### Challenge 5: Late Night Coding Session
+// Determine if it's a good idea to have a Monster Energy drink based on the current hour.
+// If it's between 7am and midnight (24-hour format), print "Unleash the beast!",
+// otherwise, print "Better stick to water."
+
+// Define the current hour in 24-hour format
+let currentHour = 22; 
+
+// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
+// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
+// Otherwise, print "Better stick to water."
+
+
+
+
+
+
+// In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
+// demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct, 
+// they should be used judiciously to maintain readability, especially for more complex conditions.
